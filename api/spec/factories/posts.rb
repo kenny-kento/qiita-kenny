7432d63810a5d9da7b68c13a893d7c3ace2d5ace
Kenty-725 <kento00coding@gmail.com>
2024-03-30 11:10:48 +0900

    rspecとfactorybotの設定修正

--- api/spec/factories/posts.rb
+++ api/spec/factories/posts.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
       title { "テストタイトル" }
       content { "rspecでテストをします" }
       user
-      # 他に必要な属性があればここに追加
     end
   end
--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
     factory :user do
       name { "山田 太郎" }
       email { "yamada@example.com" }
-      # 他に必要な属性があればここに追加
       password { "password" }
       password_confirmation { "password" }
     end
